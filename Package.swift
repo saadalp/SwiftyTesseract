1dab1566b9d93fb6809cb2138a160b9a4091f1b9
Saad Albasha <139749832+saadalp@users.noreply.github.com>
2024-10-29 15:42:20 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 #if !os(Linux)
 let dependencies: [PackageDescription.Package.Dependency] = [
-  .package(url: "https://github.com/SwiftyTesseract/libtesseract.git", .upToNextMinor(from: "0.2.0")),
+  .package(url: "https://github.com/rodonisi/libtesseract.git", .upToNextMinor(from: "0.7.0")),
 ]
 #else
 let dependencies = [PackageDescription.Package.Dependency]()
